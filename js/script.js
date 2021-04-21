a4e09ff09ea20a5706cab1cf55596cb2b8961f72
Adrian <32913236+adrigski@users.noreply.github.com>
2021-04-20 21:34:16 -0500

    change name of google maps web page and added animations

--- js/script.js
+++ js/script.js
@@ -5,48 +5,52 @@ function init(){
 } ).mount();
 }
 
-
 function initMap() {
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 4,
     center: { lat: 39.6759, lng: -101.9390 },
   });
-
   const home = new google.maps.Marker({
     position: { lat: 41.8906, lng: -87.6399 },
     map,
     title: "Chicago",
     icon: "images/home.png",
+    animation: google.maps.Animation.DROP,
   });
   const wyomingVisit = new google.maps.Marker({
     position: { lat: 42.9543, lng: -107.6443 },
     map,
     title: "Wyoming",
     icon: "images/yellowstone.png",
+    animation: google.maps.Animation.DROP,
   });
   const michiganVisit = new google.maps.Marker({
     position: { lat: 43.7871, lng: -84.7195 },
     map,
     title: "Michigan",
     icon: "images/camp.png",
+    animation: google.maps.Animation.DROP,
   });
   const wisconsinVisit = new google.maps.Marker({
     position: { lat: 44.7229, lng: -89.6151 },
     map,
     title: "Wisconsin",
     icon: "images/waterpark.png",
+    animation: google.maps.Animation.DROP,
   });
   const floridaVisit = new google.maps.Marker({
     position: { lat: 27.7169, lng: -81.6192 },
     map,
     title: "Florida",
     icon: "images/beach.png",
+    animation: google.maps.Animation.DROP,
   });
   const arizonaVisit = new google.maps.Marker({
     position: { lat: 34.4706, lng: -111.7550 },
     map,
     title: "Arizona",
     icon: "images/grandcanyon.png",
+    animation: google.maps.Animation.DROP,
   });
 
   const wyomingInfo = new google.maps.InfoWindow({
@@ -81,8 +85,5 @@ function initMap() {
     arizonaInfo.open(map, arizonaVisit);
   });
 }
-
-  var map;
-
 
 window.addEventListener('load', init);
